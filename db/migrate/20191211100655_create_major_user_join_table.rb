9b67a028404e4633b10288f80ad7a2a170174784
Emilio Wuerges <wuerges@gmail.com>
2019-12-11 07:10:34 -0300

    fixed bug on migration

--- db/migrate/20191211100655_create_major_user_join_table.rb
+++ db/migrate/20191211100655_create_major_user_join_table.rb
@@ -2,7 +2,7 @@ class CreateMajorUserJoinTable < ActiveRecord::Migration[6.0]
   def change
     create_join_table :users, :majors do |t|
       # t.index [:user_id, :major_id]
-      # t.index [:major_id, :user_id]
+      t.index [:major_id, :user_id]
     end
   end
 end

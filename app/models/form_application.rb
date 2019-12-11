0f77ba74113a26a192526f3056815245672b310d
Emilio Wuerges <wuerges@gmail.com>
2019-12-11 14:04:49 -0300

    monster sql looks ok

--- app/models/form_application.rb
+++ app/models/form_application.rb
@@ -7,11 +7,26 @@ class FormApplication < ApplicationRecord
   end
 
 
-  def gather_data(professor_id, offer_id)
+  def gather_data(professor_id, offer_id, question_type)
     # should gather data for each professor, for each offer
     # for each question, count the number of answers
-    # output a hash { question_id => [:professor_id, :offer_id, results] }
+    # output a hash { question_id => results }
     # where results = ([labels], [count], [percentage])
+
+    resp = Answer.joins("join form_submissions on answers.id = answer_id join questions on question_id = questions.id")
+                  .where("form_application_id = ?", id)
+                  .where("question_type = ?", question_type)
+                  .where("answer_type != 2")
+                  .where("professor_id = ?", professor_id)
+                  .where("offer_id = ?", offer_id)
+                  .group(["question_id", "answer_id"])
+                  .count("answer_id")
+
+    result = {}
+
+    # FormSubmission.joins("join questions q on question_id = q.id join answers a on answer_id = a.id").where("form_application_id = ?", 1).where("professor_id = ?", 1).where("a.answer_type != '2'").where("offer_id = ?", 1).where("question_id = ?", 1).group("answer_id")
+    # FormSubmission.joins("join questions q on question_id = q.id join answers a on answer_id = a.id").select("answer_id, count(answer_id) as count, a.text").where("form_application_id = ?", 1).where("professor_id = ?", 1).where("a.answer_type != '2'").where("offer_id = ?", 1).where("question_id = ?", 1).group("answer_id")
+    # Answer.joins("join form_submissions on answers.id = answer_id join questions on question_id = questions.id").select("answers.id, count(answers.id), answers.text").where("form_application_id = 1").where("question_type = 0").where("answer_type != 2").where("professor_id = 1").where("offer_id = 1").where("question_id = 1").group("answer_id")
   end
 
   def gather_complete_data

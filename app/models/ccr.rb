1d035ed2144d3dc0b59e90a4587a6f50cb6161be
Emilio Wuerges <wuerges@gmail.com>
2019-12-11 17:12:37 -0300

    validate ccr majors

--- app/models/ccr.rb
+++ app/models/ccr.rb
@@ -1,5 +1,10 @@
 class Ccr < ApplicationRecord
     has_and_belongs_to_many :majors
+    validate do |ccr|
+        if ccr.majors.size == 0
+            self.errors[:base] << "CCR has no majors"
+        end
+    end
 
     def professor_ids
         professors && professors.map(&:id)

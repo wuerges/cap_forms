a4b70077947392646a63f25dfb079032794d34dd
Emilio Wuerges <wuerges@gmail.com>
2019-12-11 12:32:21 -0300

    figuring out how to gather data

--- app/models/form_application.rb
+++ app/models/form_application.rb
@@ -5,4 +5,18 @@ class FormApplication < ApplicationRecord
   def to_s
     "#{semester.to_s} #{form_template.name}"
   end
+
+
+  def gather_data(professor_id, offer_id)
+    # should gather data for each professor, for each offer
+    # for each question, count the number of answers
+    # output a hash { question_id => [:professor_id, :offer_id, results] }
+    # where results = ([labels], [count], [percentage])
+  end
+
+  def gather_complete_data
+    # should gather data for the complete form
+    # output a hash { question_id => results }
+  end
+
 end

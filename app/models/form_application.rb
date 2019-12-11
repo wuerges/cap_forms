2ff40f75de190f29d3de43ed4bad72bb4fff9a04
Emilio Wuerges <wuerges@gmail.com>
2019-12-11 14:27:45 -0300

    try to output the tables in the views

--- app/models/form_application.rb
+++ app/models/form_application.rb
@@ -11,7 +11,7 @@ class FormApplication < ApplicationRecord
     # should gather data for each professor, for each offer
     # for each question, count the number of answers
     # output a hash { question_id => results }
-    # where results = ([labels], [count], [percentage])
+    # where results = ([labels], [count])
 
     resp = Answer.joins("join form_submissions on answers.id = answer_id join questions on question_id = questions.id")
                   .where("form_application_id = ?", id)
@@ -22,16 +22,31 @@ class FormApplication < ApplicationRecord
                   .group(["question_id", "answer_id"])
                   .count("answer_id")
 
-    result = {}
-
-    # FormSubmission.joins("join questions q on question_id = q.id join answers a on answer_id = a.id").where("form_application_id = ?", 1).where("professor_id = ?", 1).where("a.answer_type != '2'").where("offer_id = ?", 1).where("question_id = ?", 1).group("answer_id")
-    # FormSubmission.joins("join questions q on question_id = q.id join answers a on answer_id = a.id").select("answer_id, count(answer_id) as count, a.text").where("form_application_id = ?", 1).where("professor_id = ?", 1).where("a.answer_type != '2'").where("offer_id = ?", 1).where("question_id = ?", 1).group("answer_id")
-    # Answer.joins("join form_submissions on answers.id = answer_id join questions on question_id = questions.id").select("answers.id, count(answers.id), answers.text").where("form_application_id = 1").where("question_type = 0").where("answer_type != 2").where("professor_id = 1").where("offer_id = 1").where("question_id = 1").group("answer_id")
+    return fix_data(resp)    
   end
 
   def gather_complete_data
     # should gather data for the complete form
     # output a hash { question_id => results }
+    resp = Answer.joins("join form_submissions on answers.id = answer_id join questions on question_id = questions.id")
+                  .where("form_application_id = ?", id)
+                  .where("question_type = ?", question_type)
+                  .where("answer_type != 2")
+                  .group(["question_id", "answer_id"])
+                  .count("answer_id")
+    return fix_data(resp)
+  end
+
+  def fix_data(resp)
+    for k,count in resp
+      question_id, answer_id = k
+      if ! result[question_id]
+        result[question_id] = [[],[]]
+      end
+      result[question_id][0] << answer_id
+      result[question_id][1] << count
+    end
+    return result
   end
 
 end
